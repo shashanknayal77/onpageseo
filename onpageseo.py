cb68d7d6ce75050037db00f18e2cceb71bf44b0d
shashanknayal77 <134627759+shashanknayal77@users.noreply.github.com>
2024-03-29 13:42:23 +0530

    Update onpageseo.py

--- onpageseo.py
+++ onpageseo.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-import bs4
 from bs4 import BeautifulSoup
 import trafilatura
 import re
@@ -17,28 +16,30 @@ if url!="":
     if not validate_url(url):
         st.error("Please enter a valid URL")
     else:
-        st.markdown("---")
-        st.subheader("Title Analysis")
-        def get_title_length(url):
+        def get_title(url):
             try:
                 response = requests.get(url)
                 response.raise_for_status()
                 soup = BeautifulSoup(response.text, 'html.parser')
                 title = soup.title.string
-                return len(title)
+                return title
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching URL: {e}")
             except AttributeError:
                 print("Title tag not found")
             return None
 
-        title_length = get_title_length(url)
-        if title_length is not None:
-                st.write(f"Title length of {url}: {title_length}")
+        title = get_title(url)
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.subheader("**Title**")
+        with col2:
+            st.write(f"{title}")
+        if title is not None:
+                st.write(f"{len(title)} characters")
 
-        st.markdown("---")
-        st.subheader("Meta Description Analysis")
-        def find_meta_description_length(url):
+        # st.markdown("---")
+        def find_meta_description(url):
             try:
                 response = requests.get(url)
                 response.raise_for_status()
@@ -46,39 +47,53 @@ if url!="":
                 meta_description_tag = soup.find('meta', attrs={'name': 'description'})
                 if meta_description_tag:
                     content = meta_description_tag.get('content', '')
-                    return len(content)
+                    return content
                 else:
                     return 0
-
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching URL: {e}")
                 return None
+            
+        meta= find_meta_description(url)
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.subheader("**Meta Description**")
+        with col2:
+            st.write(f"{meta}")
+        if meta is not None:
+            st.write(f"{len(meta)} characters")
 
-        length = find_meta_description_length(url)
-        if length is not None:
-            st.write(f"Meta description tag length: {length}")
-
-        st.markdown("---")
-        st.subheader("Canonical Tags Analysis")
-        def count_canonical_tags(url):
+        # st.markdown("---")
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.subheader("**Url**")
+        with col2:
+            st.write(f"{url}")
+        # st.markdown("---")    
+        def canonical_tag(url):
             try:
                 response = requests.get(url)
                 response.raise_for_status()
                 soup = BeautifulSoup(response.text, 'html.parser')
-                canonical_tags = soup.find_all('link', rel='canonical')
-
-                return len(canonical_tags)
-
+                canonical_tags = soup.find('link', rel='canonical')
+                return canonical_tags
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching URL: {e}")
                 return None
 
-        canonical_tag_count = count_canonical_tags(url)
-        if canonical_tag_count is not None:
-            st.write(f"Number of canonical tags: {canonical_tag_count}")
-
-        st.markdown("---")
-        st.subheader("Language Tag Analysis")
+        canonical_tag = canonical_tag(url)
+        if canonical_tag is not None:
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                st.subheader("**Canonical**")
+            with col2:
+                st.write(f"{canonical_tag.get('href')}")
+        else:
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                st.subheader("**Canonical**")
+            with col2:
+                st.write(f"Not Specified")
         def check_lang_tag(url):
             try:
                 response = requests.get(url)
@@ -86,42 +101,106 @@ if url!="":
                 soup = BeautifulSoup(response.text, 'html.parser')
                 html_tag = soup.find('html')
                 lang_attribute = html_tag.get('lang') if html_tag else None
-                return lang_attribute is not None
+                return lang_attribute
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching URL: {e}")
                 return None
 
         has_lang_tag = check_lang_tag(url)
         if has_lang_tag is not None:
-            if has_lang_tag:
-                st.write("The webpage is using the lang tag.")
-            else:
-                st.write("The webpage is not using the lang tag.")
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                st.subheader("**Lang**")
+            with col2:
+                st.write("This page has lang tag")
+        else:
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                st.subheader("**Lang**")
+            with col2:
+                st.write(f"Not Specified")
 
-        st.markdown("---")
-        st.subheader("Header Tags Analysis")
-        def find_all_header_tags(url):
+        def check_meta_robots(url):
             try:
                 response = requests.get(url)
                 response.raise_for_status()
                 soup = BeautifulSoup(response.text, 'html.parser')
-                header_counts = {'h1': 0, 'h2': 0, 'h3': 0, 'h4': 0, 'h5': 0, 'h6': 0}
-                for header_level in header_counts.keys():
-                    header_tags = soup.find_all(header_level)
-                    header_counts[header_level] = len(header_tags)
-                return header_counts
+                meta_robots_tag = soup.find('meta', attrs={'name': 'robots'})
+                print(meta_robots_tag)
+                if meta_robots_tag:
+                    return meta_robots_tag.get('content', '')
+                else:
+                    return None
+            except requests.exceptions.RequestException as e:
+                print(f"Error fetching URL: {e}")
+                return None
+        robots_content = check_meta_robots(url)
+        if robots_content is not None:
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                st.subheader("**Meta Robots**")
+            with col2:
+                st.write(f"{robots_content}")
+        else:
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                st.subheader("**Meta Robots**")
+            with col2:
+                st.write(f"Not Specified")
+        st.subheader("Header Tags Analysis")
+        def check(url,tag):
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                soup = BeautifulSoup(response.content, 'html.parser')
+                heading_tags = soup.find_all(tag)
+                return heading_tags
+            except requests.RequestException as e:
+                print(f"Failed to fetch {url}: {e}")
 
+        h1_tag=check(url,'h1')
+        h2_tag=check(url,'h2')
+        h3_tag=check(url,'h3')
+        h4_tag=check(url,'h4')
+        h5_tag=check(url,'h5')
+        h6_tag=check(url,'h6')
+        col1,col2,col3,col4,col5,col6= st.columns([1,1,1,1,1,1])
+        with col1:
+            st.subheader("_h1_")
+            st.write(f"{len(h1_tag)}")
+        with col2:
+            st.subheader("_h2_")
+            st.write(f"{len(h2_tag)}")
+        with col3:
+            st.subheader("_h3_")
+            st.write(f"{len(h3_tag)}")
+        with col4:
+            st.subheader("_h4_")
+            st.write(f"{len(h4_tag)}")
+        with col5:
+            st.subheader("_h5_")
+            st.write(f"{len(h5_tag)}") 
+        with col6:
+            st.subheader("_h6_")
+            st.write(f"{len(h6_tag)}")  
+        def count_total_words(url):
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                soup = BeautifulSoup(response.text, 'html.parser')
+                text = soup.get_text()
+                words = re.findall(r'\b\w+\b', text.lower())
+                return len(words)
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching URL: {e}")
                 return None
 
-        header_counts = find_all_header_tags(url)
-        if header_counts is not None:
-            for header_level, count in header_counts.items():
-                st.write(f"Number of {header_level} tags: {count}")
-
-        st.markdown("---")
-        st.subheader("Alt Tags Analysis")
+        total_words = count_total_words(url)
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.subheader("**Word Count**")
+        with col2:
+            st.write(f"{total_words}")
         def check_alt_tags(url):
             try:
                 response = requests.get(url)
@@ -135,41 +214,54 @@ if url!="":
                 return None
 
         alt_tag_count = check_alt_tags(url)
-        if alt_tag_count is not None:
-            st.write(f"Number of img tags without alt attribute: {alt_tag_count}")
-
-        st.markdown("---")
-        st.subheader("Robots.txt Analysis")
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.subheader("**Images without alt attributes**")
+        with col2:
+            st.write(f"{alt_tag_count}")
+        # st.markdown("---")
+        # st.subheader("Robots.txt Analysis")
         def check_robots_txt(url):
             robots_url = url + '/robots.txt'
             try:
                 robots_txt = trafilatura.fetch_url(robots_url)
                 if robots_txt:
-                    st.write("robots.txt file found on the website.")
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        st.subheader("**Robots.txt**")
+                    with col2:
+                        st.write(f"{robots_url}")
                 elif url!="":
-                    st.write("robots.txt file not found on the website.")
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        st.subheader("**Robots.txt**")
+                    with col2:
+                        st.write(f"Not Specified")
             except requests.exceptions.RequestException as e:
                 print("Error:", e)
 
         check_robots_txt(url)
-
-        st.markdown("---")
-        st.subheader("Sitemap.xml Analysis")
         def check_sitemap(url):
             sitemap_url = url + '/sitemap.xml'
             try:
                 sitemap = trafilatura.fetch_url(sitemap_url)
                 if sitemap:
-                    st.write("sitemap.xml file found on the website.")
-                elif url !="":
-                    st.write("sitemap.xml file not found on the website.")
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        st.subheader("**Sitemap.xml**")
+                    with col2:
+                        st.write(f"{sitemap_url}")
+                elif url!="":
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        st.subheader("**Sitemap.xml**")
+                    with col2:
+                        st.write(f"Not Specified")
             except requests.exceptions.RequestException as e:
                 print("Error:", e)
 
         check_sitemap(url)
-
-        st.markdown("---")
-        st.subheader("Text to HTML Ratio Analysis")
+        # st.subheader("Text to HTML Ratio Analysis")
         def get_text_to_html_ratio(url):
             try:
                 response = requests.get(url)
@@ -184,11 +276,19 @@ if url!="":
                 return None
 
         text_to_html_ratio = get_text_to_html_ratio(url)
-        if text_to_html_ratio is not None:
-            st.write(f"Text to HTML ratio for {url}: {text_to_html_ratio:.2f}")
-
-        st.markdown("---")
-        st.subheader("URL Length Analysis")
-        st.write(f"The length of URL is {len(url)}")
+        # if text_to_html_ratio is not None:
+        #     st.write(f"Text to HTML ratio for {url}: {text_to_html_ratio:.2f}")
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.subheader("**Text To Html Ratio**")
+        with col2:
+            st.write(f"{text_to_html_ratio:.2f}")
+        # st.markdown("---")
+        # st.subheader("URL Length Analysis")
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.subheader("**Url length**")
+        with col2:
+            st.write(f"{len(url)}")
 else:
     st.info("Enter a URL to analyze")
